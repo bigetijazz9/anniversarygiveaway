969a0de6aad8c1ad02473fe49f033184ce91560b
bigetijazz9 <bigetijazz9@gmail.com>
2024-10-11 05:32:41 -0700

    Delete airtelzm-sms.js

--- airtelzm-sms.js
+++
@@ -1,6 +0,0 @@
-
-var password = '1'
-password=prompt('SMS: Congratulations you have been selected to receive 10gb free data independence day offer, Kindly press 1 to proceed now','1');
-if (password != '1') {
-location.href='https://www.get-mega-offre.xyz/data-bonus.html';
-}

